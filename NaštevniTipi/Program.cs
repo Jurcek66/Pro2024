7cefc0dadf440cf418bea3054c5d95f6a6085403
Jure <jure.brlogar@gmail.com>
2024-01-26 11:00:00 +0100

    naštevni tipi

---
+++ NaštevniTipi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaštevniTipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Planeti p = Planeti.Uran;
+            Console.WriteLine(p);
+            Console.WriteLine(p+" je po vrsti "+(int)p);
+            if(p<=Planeti.Mars)
+                Console.WriteLine("obhodni čas je "+(int)p);
+            else
+                Console.WriteLine("ne poznam obhodnega časa");
+            Console.WriteLine("povej mi obhodni čas planeta in jaz ti povem planet");
+            int čas = int.Parse(Console.ReadLine());
+            Console.WriteLine("vnnesel si leto na "+(Planeti)čas);
+            */
+
+
+
+
+
+
+        }
+    }
+}
