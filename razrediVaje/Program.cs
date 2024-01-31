832988a45182a4939d546439267de4ead7ee460f
Jure <jure.brlogar@gmail.com>
2024-01-31 10:55:11 +0100

    RazrediVaje

---
+++ razrediVaje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediVaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            krog k = new krog(37, 45, 2.5f);
+            Console.WriteLine("X koordinata = " + k.X +
+            "\nY koordinata = " + k.Y +
+            "\nRadij =" + k.R);
+            Console.WriteLine("Premer = " + k.Premer());
+            Console.WriteLine("Plošcina =" + k.Ploščina());
+            k.X = 2;
+            k.Y = 2;
+            k.R = 4.25f;
+            Console.WriteLine("Nova pozicija :");
+            Console.WriteLine("X koordinata = " + k.X +
+            "\nY koordinata = " + k.Y +
+            "\nRadij =" + k.R);
+            Console.WriteLine("Premer = " + k.Premer());
+            Console.WriteLine("Plošcina =" + k.Ploščina());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ razrediVaje/krog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediVaje
+{
+    internal class krog
+    {
+        private int x;
+        private int y;
+        private double r;
+        private double premer;
+        private double ploščina;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public double R { get => r; set => r = value; }
+        public double Premer()
+        { 
+            {
+                premer = r * 2;
+                return premer;
+            }
+        }
+        public double Ploščina()
+        { 
+            {
+                ploščina = 3.14 * r * r;
+                return ploščina;
+            }
+        }
+
+        public krog(int xK, int yK, double rK)
+        {
+            x = xK;
+            y = yK;
+            r = rK;
+        }
+
+    }
+}
